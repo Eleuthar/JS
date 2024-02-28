69c70445d1c1094ab94abbd2bed46bf44fea71d0
Eleuthar <flbujoreanu@gmail.com>
2024-02-28 11:22:17 +0200

    Update csv_number_field.js

--- csv_number_field.js
+++ csv_number_field.js
@@ -1,86 +1,71 @@
-// 'allow' number field to dynamically behave like a text field, allowing thousand comma separator
-// on document load, convert number input type to 'text'
-// add event listener for converted input field to remove NaN char including ","
-// add on submit form event listener
-// on valid form submit, transform silently the number field value to allow backend validation as decimal \ float
-
-
-// function validate_form(change_form) {
-//     for (let input of document.querySelectorAll(`#${change_form.id} input:not([type="submit"])`)) {}
-// }
+// remove non numeric characters except "."
+function sanitize_number(num) {
+    let integer = [];
+    if (isNaN(parseInt(num))) {
+        // remove isNaN part
+        for (let ndx = 0; ndx <= num.length; ndx++) {
+            if (!isNaN(num[ndx])) {
+                integer.push(num[ndx]);
+            }
+        }
+        if (integer.length > 0) {
+            return parseInt(integer.join('')).toLocaleString();
+        }
+        return '';
+    }
+    return parseInt(num).toLocaleString();
+}
 
 // make text field behave like a number field
-function number_input_behaviour(integer, dot, decimals, max_decimal_length) {
-    if (dot) {
-        let csv = integer.toLocaleString;
-        // full number including decimal digits
-        let decimal = decimals.length > 0 ? csv + '.' + decimals.join('') : csv;
-        let char_ndx = decimal.length - 1;
-        let splitted = decimal.split('');
-        let num_len = decimals.length;
-
-        // delete extra decimal digit
-        if (num_len == max_decimal_length) {
-            splitted.splice(char_ndx, 1);
-        }
-        // add decimal digit if valid
-        else if (num_len < max_decimal_length) {
-            let char = decimal[char_ndx];
-            if (isNaN(char)) {
-                splitted.splice(char_ndx, 1);
-            }
-            else {
-                decimals.push(char);
-            }
-        }
+// remove comma and extra dots from num to allow conversion to float
+function number_input_behaviour(input, max_decimal_length) {
+    // [ "3", "4,5", "67,6", "7", "", "", "77", "", "", "", "666"].join('.')
+    const splitted = pin.value.split('.');
+    // value has no decimal
+    if (splitted.length == 1) {
+        // remove potential commas, as they act as decimal
+        // parseFloat & parseInt removes the part beyond ","
+        let integer = splitted[0].split(',').join('');
+        return sanitize_number(integer, splitted.length);
     }
-    else {
-        let char_ndx = integer.length - 1;
-        let char = integer[char_ndx];
-        // remove automagically character if NaN 
-        if (isNaN(char)) {
-            if ("." != char) {
-                let splitted = integer.split('');
-                splitted.splice(ndx, 1);
-                csv = parseInt(integer.join('')).toLocaleString();
-            }
-            // only 2-digit decimals allowed
-            else if (char == '.') {
-                // store dot index
-                if (!dot) {
-                    dot = True;
-                }
-            }
+    else if (splitted.length >= 2) {
+        let [integer, decimals] = splitted;
+        for (let num of [integer, decimals]) {
+            num = num.split(',').join('');
+            num = sanitize_number(num, splitted.length);
         }
-    }    
-    return csv, integer, dot, decimals;
+        if (decimals.length > max_decimal_length) {
+            decimals = decimals.slice(0, max_decimal_length);
+        }
+        else if (decimals == '') {
+            decimals == '00';
+        }
+        return integer + '.' + decimals;
+    }
+}
+ 
+function convert_to_text_field(field) {
+    let dot_quant = 0;
+    const max_decimal_length = 2;
+    field.type = 'text';
+    field.placeholder = `${max_decimal_length}-digit floating point values only!`
+    field.title = `${max_decimal_length}-digit floating point values only!`
+    if (field.value != '') {
+        field.value = parseFloat(field.value).toLocaleString();
+    }
+    field.addEventListener('keyup', function(event) {
+        if (!['Backspace', 'Delete'].includes(event.key )) {
+            this.value = number_input_behaviour(field, max_decimal_length);
+        }
+    });
 }
 
-// remove comma to allow conversion to float
-function to_decimal(num) {
-    for (let q = 0; q <= num.length; q++) {
-        if (num[q] == ',') {
-            num.splice(q, 1);
-        }
-    }
-    return num.join('');
-}
-
-$( document ).ready(function() {
-    let dot = False;
-    let decimals = [];
-    let csv = null;
+$( document ).ready(function() { 
     const change_form = document.querySelector('#content-main > form');
     const inhabitants = document.getElementById('id_inhabitants');
-    inhabitants.type = 'text';
-    // 
-    inhabitants.addEventListener('keyup', function(evt) {
-        [csv, integer, dot, decimals] = number_input_behaviour(integer, dot, decimals, max_decimal_length);
-    });
-
+    convert_to_text_field(inhabitants);
     change_form.addEventListener('submit', function(event) {
-        buffered = to_decimal(inhabitants.value.split(''));
+        inhabitants.value = inhabitants.value.split(',').join('');
         document.getElementById('id_inhabitants').type = 'number';
-        inhabitants.value = buffered;
     });
 });
